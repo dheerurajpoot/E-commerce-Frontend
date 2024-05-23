39d662afd0a6ae263ac60a836da3807380f0c873
Dheeru Rajpoot <150139748+dheerurajpoot@users.noreply.github.com>
2024-05-23 20:57:54 +0530

    gettting attack

--- src/utils/base_url.jsx
+++ src/utils/base_url.jsx
@@ -1,1 +1,2 @@
-export const base_url = "https://e-commerce-backend-xb1p.onrender.com/api/";
+// export const base_url = "https://e-commerce-backend-xb1p.onrender.com/api/";
+// export const base_url = "http://localhost:3000/api/";
