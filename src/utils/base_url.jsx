3d1b838aaa9d2609aca5560be7dd3c028d9658ef
Dheeru Rajpoot <150139748+dheerurajpoot@users.noreply.github.com>
2024-05-23 21:00:00 +0530

    due to attack site down

--- src/utils/base_url.jsx
+++ src/utils/base_url.jsx
@@ -1,2 +1,2 @@
 // export const base_url = "https://e-commerce-backend-xb1p.onrender.com/api/";
-// export const base_url = "http://localhost:3000/api/";
+export const base_url = "http://localhost:3000/api/attack";
